fc1ec2daa91a276928393fc22083f983c319c1b8
Arthur Cortez <t.multimidia4@al.senai.br>
2024-05-14 04:48:09 -0300

    fix: removed unused import

--- src/main/java/com/arthurcortez/javaproject/service/UserService.java
+++ src/main/java/com/arthurcortez/javaproject/service/UserService.java
@@ -9,7 +9,6 @@ import com.arthurcortez.javaproject.security.TokenService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
